bb63e58d0b7d1a76d446e46cf028e627ae1a59a3
mgonzalezmesa <33700123+mgonzalezmesa@users.noreply.github.com>
2017-12-28 15:42:37 +0100

    Add files via upload

--- cuestionario/vistaAcceso.php
+++ cuestionario/vistaAcceso.php
@@ -14,13 +14,14 @@ class vistaAcceso{
             <div style="">   <input type="text" name="nombre" style="font-family: 'Roboto', sans-serif;outline: 0;background: #f2f2f2;width: 100%; border: 0;margin: 0 0 15px;padding: 15px;box-sizing: border-box;font-size: 14px;" placeholder="&#128100; &nbsp;&nbsp;Nombre de usuario" required /></div>
             <div style="    width: 92%;background: #f2f2f2; padding: 15px;"><label  >Dificultad</label></div>
 			<div class="caja">   
-                <select name="dificultad" style="background: transparent;border: none; font-size: 14px;height: 30px;padding: 5px;width: 270px;">
+                <select name="dificultad" style="background: #ffffff;border: none; font-size: 14px;height: 30px;padding: 5px;width: 270px;">
                 <option value="facil">Fácil</option>
                 <option value="normal">Normal</option>
                 <option value="dificil">Difícil</option>
             </select>
                 </div>
-			<input style="padding:15px; width:40%;" type="submit" name="empezar" value="Empezar"/>
+			 <!--<input style="padding:15px; width:40%;" type="submit" name="empezar" value="Empezar" class="boton"/>-->
+             <button class="button" style="vertical-align:middle" name="empezar" value="Empezar"><span>EMPEZAR </span></button>
         </form>
 		</div>
         <div id="clear" style="clear:both;"></div>
